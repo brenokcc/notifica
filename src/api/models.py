0b75bb782239417eb194f6b7f26b688f7fc4d31b
Breno Silva <brenokcc@yahoo.com.br>
2026-01-23 09:09:26 -0300

    Solicitação de cadastro com e-mail diferente

--- src/api/models.py
+++ src/api/models.py
@@ -590,6 +590,7 @@ class SolicitacaoCadastro(models.Model):
 
     @transaction.atomic
     def processar(self):
+        definir_senha = not User.objects.filter(username=self.cpf).exists()
         model = {'gm': GestorMunicipal, 'gu': GestorUnidade, 'notificante': Notificante, 'agente': Agente, 'regulador': Regulador, 'supervisor': Supervisor}[self.papel]
         obj = (
             model.objects.filter(cpf=self.cpf).first() or model()
@@ -627,8 +628,8 @@ class SolicitacaoCadastro(models.Model):
                 self.municipio.reguladores.add(obj)
                 self.municipio.post_save()
             password = None
-            user = User.objects.filter(username=self.cpf).first()
-            if not user.token_set.exists():
+            if definir_senha:
+                user = User.objects.filter(username=self.cpf).first()
                 password = '123' if settings.DEBUG else uuid1().hex[0:6]
                 user.set_password(password)
                 user.save()
