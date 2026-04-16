6ef0d16ca9269f6f3793e64ef34d1b0c61feef9c
Breno Silva <brenokcc@yahoo.com.br>
2026-04-16 20:00:53 -0300

    Endpoint para integração com a telefiocruz para fins de monitoramento dos pacientes positivados

---
+++ src/api/endpoints/acompanhamento.py
@@ -0,0 +1,23 @@
+from slth import endpoints
+from ..models import *
+from datetime import date, timedelta
+from slth.serializer import serialize
+from django.conf import settings
+
+
+class Fila(endpoints.Endpoint):
+
+    def get(self):
+        return [
+            {k:serialize(v) for k, v in x.items()} for x in 
+            NotificacaoIndividual.objects
+            .filter(status_infeccao='Positivo', doenca=2, unidade_referencia__codigo=self.request.GET['cnes'], data_primeiros_sintomas__gte=date.today() - timedelta(days=145))
+            .values('nome', 'cpf', 'cartao_sus', 'sexo__nome', 'data_nascimento', 'email', 'telefone', 'ocupacao_investigacao__nome')
+            .order_by('data_primeiros_sintomas')[0:2]
+        ]
+    
+    def check_permission(self):
+        if 'HTTP_AUTHORIZATION' in self.request.META:
+            token = self.request.META['HTTP_AUTHORIZATION'].split()[1]
+            return token == settings.SECRET_KEY[-20:]
+        return False
